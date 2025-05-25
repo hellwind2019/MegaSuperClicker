4f09b4d78fc85e76670e638c172e084131694cc0
Dania <daniltorianik@gmail.com>
2025-05-25 19:49:25 +0300

    Add button

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello world")
+import sys
+from PyQt5.QtWidgets import *
+
+class Clicker(QWidget):
+    def __init__(self) ->None:
+        super().__init__()
+        self.init_ui()
+        self.show()
+
+    def init_ui(self):
+        ...
+        #Тут створити layout Та кнопки
+
+
+app = QApplication(sys.argv)
+window = Clicker()
+sys.exit(app.exec_())
